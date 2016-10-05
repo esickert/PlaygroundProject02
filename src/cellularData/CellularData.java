97ebb8c435ff61f382d0c70ba475ba7155d75fc2
esickert <esickert@gmail.com>
2016-10-05 15:03:00 -0700

    more

--- src/cellularData/CellularData.java
+++ src/cellularData/CellularData.java
@@ -23,19 +23,14 @@ public class CellularData {
 		anArray = new Object[numRows][numColumns];  //this is an array of Object class
 	}
 	
-	
-	
 	public void addCountry(String country, double[] a)	{
-		
+
 			anArray[0][0] = country;
 			for (int i = 1; i <= a.length - 1; i++)	{
 					anArray[0][i] = a[i];
-			}  
+			}
 	}  
-//		
-	
-	
-	
+	  
 	public String toString()	{ //this works to override java.lang.Object.toString
 		String output = "";
 		for(int i = 0; i <= anArray.length - 1; i++)	{
