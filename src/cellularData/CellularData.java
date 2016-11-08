b8e0f03d3f2623bde00635d4f4b757a2aeb7a91c
esickert <esickert@gmail.com>
2016-11-07 17:04:41 -0800

    more

--- src/cellularData/CellularData.java
+++ src/cellularData/CellularData.java
@@ -61,30 +61,12 @@ public class CellularData {
 //		}  
 		System.out.println();  //add lines between output
 				
-		System.out.println(country + ": " + decimalPlaces.format(temp) + "\n");
+//		System.out.println(country + ": " + decimalPlaces.format(temp) + "\n");
 		return temp;
 	}
 				
 	 
 //*****************************************************************************************		
-
-/*	public void AddYear(String[][] a)	{
-	
-	//    int startingYear = 1983;
-    	int endingYear = 1987;
-//    	int yearGap = endingYear - startingYear;                                                                        //this works
-    	for (int i = 1; i < a[0].length - 1; i++)    {  //there is an oject -type problem here!!!!!ARRAYS ARE NOW STRINGS.
-//      	int foo = Integer.parseInt("1234");
-        	int b = Integer.parseInt(a[0][i]);          //string parsed to integer
-        	System.out.println();
-       		if (b == 1987)    {
- //           	System.out.println("The index of 1987 is " + i);
-            	break;
-        	}
-//        else System.out.println(a + " is not in the array!!!!");
-    }
-	}
-*/
 
 	public void addCountry(String country, double[] a)	{          //THE ARRAY WILL STORE DATA AS A STRING THEN CONVERT INT STRINGS TO INTEGERS.
 	//BE SURE TO REINITIALIZE THE ARRAY FOR NEW DATASET!!!!!!!!!!!!!!!!!!!!!!USING "NEW ARRAY STRING[][]"	
